43a2cc50bafc6511cda7b8b7bc85b33a0463869f
Masao KATO <ponkore@gmail.com>
2015-12-29 22:45:46 +0900

    many changes .. preparing 1st alpha release.

--- project.clj
+++ project.clj
@@ -8,8 +8,9 @@
                            [compojure "1.4.0"]
                            [ring-server "0.4.0"]
                            ;; [cryogen-markdown "0.1.1"]
+                           [com.darrinholst/sass-java "3.4.20.0" :exclusions [com.github.jnr/jffi com.github.jnr/jnr-x86asm]]
                            [cryogen-asciidoc "0.1.2"]
-                           [cryogen-core "0.1.27"]]
+                           [cryogen-core "0.1.28-SNAPSHOT"]] ;; "0.1.27"
             :plugins [[lein-ring "0.9.6"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
--- src/cryogen/server.clj
+++ src/cryogen/server.clj
@@ -2,6 +2,7 @@
   (:require [compojure.core :refer [GET defroutes]]
             [compojure.route :as route]
             [ring.util.response :refer [redirect]]
+            [ring.adapter.jetty :refer [run-jetty]]
             [cryogen-core.watcher :refer [start-watcher!]]
             [cryogen-core.plugins :refer [load-plugins]]
             [cryogen-core.compiler :refer [compile-assets-timed read-config]]))
@@ -12,7 +13,23 @@
   (let [ignored-files (-> (read-config) :ignored-files)]
     (start-watcher! "resources/templates" ignored-files compile-assets-timed)))
 
+(defn destroy []
+  )
+
+(defonce server (atom nil))
+
 (defroutes handler
   (GET "/" [] (redirect (str (:blog-prefix (read-config)) "/index.html")))
   (route/resources "/")
   (route/not-found "Page not found"))
+
+(defn start-server
+  []
+  (init)
+  (when-not @server
+    (reset! server (run-jetty handler {:port 3000 :join? false}))))
+
+(defn stop-server []
+  (when @server
+    (.stop @server)
+    (reset! server nil)))
