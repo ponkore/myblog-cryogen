adf116d5560b381dfc0b13c75c35be4ae0f80718
ponkore <ponkore@gmail.com>
2016-07-02 00:30:07 +0900

    core version up
    Signed-off-by: ponkore <ponkore@gmail.com>

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                            [ring-server "0.4.0"]
                            [com.darrinholst/sass-java "3.4.20.0" :exclusions [com.github.jnr/jffi com.github.jnr/jnr-x86asm]]
                            [cryogen-asciidoc "0.1.2"]
-                           [cryogen-core "0.1.29-SNAPSHOT"]] ;; "0.1.28"
+                           [cryogen-core "0.1.41"]]
             :plugins [[lein-ring "0.9.6"]]
             :main cryogen.core
             :repl-options {
